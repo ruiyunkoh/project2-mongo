4fc301feeae6f10eb9fc58caab6098547de21785
ruiyunkoh <ruiyun.k@gmail.com>
2022-01-12 14:52:42 +0000

    enabled cors

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 require("dotenv").config();
+const cors = require("cors");
 
 const ObjectId = require("mongodb").ObjectId;
 const MongoUtil = require("./MongoUtil");
@@ -9,10 +10,17 @@ const mongoUrl = process.env.MONGO_URL;
 let app = express();
 app.use(express.json());
 
+app.use(cors());
 
 async function main() {
 
   let db = await MongoUtil.connect(mongoUrl, "exercise_list");
+
+  app.get("/", function(req, res){
+    res.json({
+      "message": "Hello"
+    })
+  })
 
 }
 
