8b22080dc580fbb6925e5ec3c13f1947bc15aa49
ruiyunkoh <ruiyun.k@gmail.com>
2022-01-12 18:21:16 +0000

    added post method

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 const ObjectId = require("mongodb").ObjectId;
 const MongoUtil = require("./MongoUtil");
 
-const mongoUrl = process.env.MONGO_URL;
+const mongoUrl = process.env.MONGO_URI;
 
 let app = express();
 app.use(express.json());
@@ -14,14 +14,65 @@ app.use(cors());
 
 async function main() {
 
-  let db = await MongoUtil.connect(mongoUrl, "exercise_list");
+ await MongoUtil.connect(mongoUrl, "exercise_list");
 
-  app.get("/", function(req, res){
-    res.json({
-      "message": "Hello"
-    })
-  })
+  // app.get("/", function(req,res){
+  //   res.json({
+  //     "message":"hello"
+  //   })
+  // })
+  
+  //Add in new exercise POST
+  app.post("/new_exercise", async (req, res) => {
+    const db = MongoUtil.getDB();
 
+    let {poster, title, image, duration, description, routine, type, intensity, target_area, calories_burnt, tags} = req.body;
+    if (!Array.isArray(tags)) {
+      tags = [tags];
+    }
+
+    // let poster = req.body.poster;
+    // let title = req.body.title;
+    // let image = req.body.image;
+    // let duration = req.body.duration;
+    // let description = req.body.description;
+    // let routine = req.body.routine;
+    // let type = req.body.type;
+    // let intensity = req.body.intensity;
+    // let target_area = req.body.target_area;
+    // let calories_burnt = req.body.calories_burnt;
+    // let tags = req.body.tags;
+
+
+    let result = await db.collection("exercises").insertOne({
+      poster, 
+      title, 
+      image,
+      duration,
+      description,
+      routine,
+      type, 
+      intensity,
+      target_area,
+      calories_burnt,
+      tags
+      
+      // "poster": poster,
+      // "title": title,
+      // "image": image,
+      // "duration": duration,
+      // "description": description,
+      // "routine": routine,
+      // "type": type,
+      // "intensity": intensity,
+      // "target area": target_area,
+      // "calories burnt": calories_burnt,
+      // "tags": tags
+
+    });
+    res.json(result);
+
+  });
 }
 
 main();
